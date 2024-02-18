72066af9541ce97a7f19beb71dbf07db7fa83f87
Vitor Mancini <vitor.mancini21@hotmail.com>
2024-02-18 12:48:52 -0300

    feat: implementing error handler

---
+++ src/errors/error_handler.py
@@ -0,0 +1,11 @@
+from src.views.http_types.http_response import HttpResponse
+
+def handle_errors(error: Exception) -> HttpResponse:
+    return HttpResponse(
+        status_code=500, 
+        body={
+            'errors': [{
+                'title': 'Server error',
+                'deatail': str(error)
+            }]
+    })
--- src/routes/tag_routes.py
+++ src/routes/tag_routes.py
@@ -1,13 +1,18 @@
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
+from src.errors.error_handler import handle_errors
 
 tags_routes_bp = Blueprint('tags_roues', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=['POST'])
 def create_tags():
-    http_request = HttpRequest(body = request.json) # Obtém o corpo da requisição
-    tag_creator_view = TagCreatorView()
-    response = tag_creator_view.validate_and_create(http_request) # Invoca o serviço de validação e criação de tags
+    response = None
+    try:
+        http_request = HttpRequest(body = request.json) # Obtém o corpo da requisição
+        tag_creator_view = TagCreatorView()
+        response = tag_creator_view.validate_and_create(http_request) # Invoca o serviço de validação e criação de tags
+    except Exception as e:
+        response = handle_errors(e)
 
     return jsonify(response.body), response.status_code
